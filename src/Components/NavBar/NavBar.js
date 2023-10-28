fbe67d04ea3e44ad6859474ee2d9d1e41ab3edaa
Gaurav <gauravverma3646@gamil.com>
2023-10-29 03:01:47 +0530

    quickshell assignment first commit

---
+++ src/Components/NavBar/NavBar.js
@@ -0,0 +1,7 @@
+import React from "react";
+import classes from "./NavBar.module.css";
+const NavBar = () => {
+  return <div className={classes.navBarContainer}>Hello</div>;
+};
+
+export default NavBar;
